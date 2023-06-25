e04e9f0cd6b7be4181cb2a12a66c9338f891af7c
kiheino <kihei.1281.nana@gmail.com>
2023-06-26 07:12:57 +0900

    Delete Dupulicated Client　Files

--- api/clientAPI.js
+++
@@ -1,9 +0,0 @@
-import axiosClient from "./axiosClient";
-
-const clientApi = {
-  register: (client) => axiosClient.post("client/register", client),
-  getClient: (clientName) => axiosClient.get(`client/${clientName}`),
-  getAll: () => axiosClient.get("/client"),
-};
-
-export default clientApi;
